37dbe50e67b7dc6182e3a10eb6ceeb5669e2efeb
Wil Gieseler <wil@wilgieseler.com>
2014-01-23 13:13:51 -0800

    can resolve commons media images

--- lib/wikidata/datavalues/commons_media.rb
+++ lib/wikidata/datavalues/commons_media.rb
@@ -6,6 +6,12 @@ module Wikidata
         data_hash.imagename
       end
 
+      def resolve!
+        query = {image: data_hash.imagename, thumbwidth: 150}
+        r = HTTParty.get('http://tools.wmflabs.org/magnus-toolserver/commonsapi.php', {query: query})
+        @data_hash = Hashie::Mash.new(r['response'].merge({imagename: data_hash.imagename}))
+      end
+
     end
   end
 end
